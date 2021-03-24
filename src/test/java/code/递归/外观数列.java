7190ad4e2d6d775c4b1d185794dc972e72baf8c9
mocas <wangyuhang_mocas@163.com>
2021-03-24 11:35:44 +0800

    测试push

--- src/test/java/code/递归/外观数列.java
+++ src/test/java/code/递归/外观数列.java
@@ -6,6 +6,8 @@ package code.递归;/**
  * @email: wangyuhang_mocas@163.com
  */
 
+import org.junit.Test;
+
 /**
  *@program: AlgorithmCode
  *@description:
@@ -13,4 +15,48 @@ package code.递归;/**
  *@create: 2020-11-03 16:08
  */
 public class 外观数列 {
+    //递归
+    public  String countAndSay(int n)
+    {
+        String s="";//用来描述上一个字符串
+        StringBuilder str=new StringBuilder();//用来构建现在的字符串
+        //终止条件
+        if (n==1)
+        {
+            return "1";
+        }
+        //上一个字符串
+        s=countAndSay(n-1);
+        int len=s.length();
+        int pre=0;
+        int count=0;
+        int i=0;
+        for (;i<s.length();i++)
+        {
+           //如果不相等，则更新计数
+            if(s.charAt(pre)!=s.charAt(i))
+            {//如果不相等时，更新指针
+                count=i-pre;
+                str.append(count).append(s.charAt(pre));
+                pre=i;
+
+            }
+            //相等则更新i
+        }
+        //如果更新到最后截止
+        if (pre!=i)
+        {
+            count=i-pre;
+            str.append(count).append(s.charAt(pre));
+        }
+        return str.toString();
+
+    }
+
+    @Test
+    public void test()
+    {
+        String rs=countAndSay(6);
+        System.out.println(rs);
+    }
 }
